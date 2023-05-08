55d902df7e798c7b7803a602d6eaa65236274e3d
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-05-08 13:25:34 +0200

    Represent `Score` as a single byte
    Replace the old [u8; 5] with a single u8.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -25,7 +25,7 @@ fn main() {
         board.input = read_input(&mut input).unwrap();
         println!();
 
-        won = is_win(&board.score());
+        won = board.score().is_win();
 
         println!("{}", board);
 
@@ -64,26 +64,21 @@ fn pick_random_word(random_state: &mut Rand) -> Word {
 enum LetterScore {
     /// A letter that is not in the word at all.
     #[default]
-    Wrong,
+    Wrong = 0,
     /// A letter that is in the word but at a different spot.
-    InWord,
+    InWord = 1,
     /// A letter both in the word and at the right spot.
-    Right,
+    Right = 2,
 }
 
 impl LetterScore {
-    const WRONG: u8 = 0;
-    const IN_WORD: u8 = 1;
-    const RIGHT: u8 = 2;
+    const ALL: [Self; 3] = [Self::Wrong, Self::InWord, Self::Right];
 
     // this is obviously unfortunate compared to just assigning the variants directly
     // but should the variants ever need special values this will come in handy.
+    #[cfg(test)]
     const fn variant(self) -> u8 {
-        match self {
-            Self::Wrong => Self::WRONG,
-            Self::InWord => Self::IN_WORD,
-            Self::Right => Self::RIGHT,
-        }
+        self as u8
     }
 
     const fn bg_color(self) -> u8 {
@@ -96,20 +91,39 @@ impl LetterScore {
 }
 
 /// Renders `word` to `w` given `score`. Uses ANSI escapes to color the letters.
-fn render(word: &Word, score: &Score, mut w: impl Write) -> io::Result<()> {
-    for (c, s) in word.iter().zip(score) {
-        let color = s.bg_color();
+fn render(word: &Word, score: Score, mut w: impl Write) -> io::Result<()> {
+    for (i, c) in word.iter().enumerate() {
+        let color = score.get(i).bg_color();
         write!(w, "\x1b[30;{color}m{0}", *c as char)?;
     }
     write!(w, "\x1b[m")?;
     Ok(())
 }
 
-type Score = [LetterScore; 5];
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+struct Score(u8);
+
+impl Score {
+    const PERFECT: Self = Score(242);
+
+    fn is_win(self) -> bool {
+        self == Self::PERFECT
+    }
+
+    fn set(&mut self, at: usize, score: LetterScore) {
+        let pos = 3u8.pow(at as u32);
+        self.0 += score as u8 * pos;
+    }
+
+    fn get(self, at: usize) -> LetterScore {
+        let pos = 3u8.pow(at as u32);
+        LetterScore::ALL[(self.0 / pos) as usize % 3]
+    }
+}
 
 fn score(word: &Word, guess: &Word) -> Score {
     if word == guess {
-        return [LetterScore::Right; 5];
+        return Score::PERFECT;
     }
 
     // invalid ascii byte to use as a placeholder
@@ -120,58 +134,22 @@ fn score(word: &Word, guess: &Word) -> Score {
     // find correct letters
     for (i, (w, g)) in word.iter_mut().zip(guess).enumerate() {
         if w == g {
-            score[i] = LetterScore::Right;
+            score.set(i, LetterScore::Right);
             *w = SENTINEL;
         }
     }
 
-    for (g, s) in guess.iter().zip(score.iter_mut()) {
-        if *s == LetterScore::Right {
+    for (i, g) in guess.iter().enumerate() {
+        if score.get(i) == LetterScore::Right {
             continue;
         }
         if let Some(w) = word.iter_mut().find(|w| *w == g) {
-            *s = LetterScore::InWord;
+            score.set(i, LetterScore::InWord);
             *w = SENTINEL;
         }
     }
 
     score
-}
-
-/// `compress` compresses a `Score` into a single byte.
-/// This works out because log2(3^5) < 8.
-///
-/// [`decompress`] reverses this process again.
-#[allow(unused)]
-fn compress(score: &Score) -> u8 {
-    let mut compressed = 0;
-    for s in score.iter() {
-        compressed *= 3;
-        compressed += s.variant();
-    }
-    compressed
-}
-
-/// `decompress` reverses the [`compress`] process.
-#[allow(unused)]
-fn decompress(mut score: u8) -> Score {
-    let mut decompressed = Score::default();
-    for i in 0..decompressed.len() {
-        let ls = match score % 3 {
-            LetterScore::WRONG => LetterScore::Wrong,
-            LetterScore::IN_WORD => LetterScore::InWord,
-            LetterScore::RIGHT => LetterScore::Right,
-            _ => unreachable!(),
-        };
-        decompressed[decompressed.len() - 1 - i] = ls;
-        score /= 3;
-    }
-    decompressed
-}
-
-/// Determines whether a score is winning.
-fn is_win(score: &Score) -> bool {
-    score.iter().all(|&s| s == LetterScore::Right)
 }
 
 struct Board {
@@ -204,7 +182,7 @@ impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut s: Vec<u8> = Vec::new();
         for (word, score) in &self.guesses {
-            render(&word, &score, &mut s).expect("OOM");
+            render(word, *score, &mut s).expect("OOM");
             s.push(b'\n');
         }
 
@@ -247,26 +225,81 @@ impl Default for Rand {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_score() {
         use LetterScore::*;
         let table = [
-            (b"words", b"birds", [Wrong, Wrong, Right, Right, Right]),
-            (b"tests", b"stabs", [InWord, InWord, Wrong, Wrong, Right]),
-            (b"cargo", b"gocar", [InWord; 5]),
-            (b"cargo", b"cargo", [Right; 5]),
-            (b"stark", b"lossy", [Wrong, Wrong, InWord, Wrong, Wrong]),
-            (b"liege", b"liens", [Right, Right, Right, Wrong, Wrong]),
-            (b"liege", b"litre", [Right, Right, Wrong, Wrong, Right]),
-            (b"abcde", b"edcba", [InWord, InWord, Right, InWord, InWord]),
-            (b"abcde", b"ccccc", [Wrong, Wrong, Right, Wrong, Wrong]),
-            (b"abcde", b"ccxxx", [InWord, Wrong, Wrong, Wrong, Wrong]),
+            (
+                b"words",
+                b"birds",
+                [Wrong, Wrong, Right, Right, Right].into(),
+            ),
+            (
+                b"tests",
+                b"stabs",
+                [InWord, InWord, Wrong, Wrong, Right].into(),
+            ),
+            (b"cargo", b"gocar", [InWord; 5].into()),
+            (b"cargo", b"cargo", [Right; 5].into()),
+            (
+                b"stark",
+                b"lossy",
+                [Wrong, Wrong, InWord, Wrong, Wrong].into(),
+            ),
+            (
+                b"liege",
+                b"liens",
+                [Right, Right, Right, Wrong, Wrong].into(),
+            ),
+            (
+                b"liege",
+                b"litre",
+                [Right, Right, Wrong, Wrong, Right].into(),
+            ),
+            (
+                b"abcde",
+                b"edcba",
+                [InWord, InWord, Right, InWord, InWord].into(),
+            ),
+            (
+                b"abcde",
+                b"ccccc",
+                [Wrong, Wrong, Right, Wrong, Wrong].into(),
+            ),
+            (
+                b"abcde",
+                b"ccxxx",
+                [InWord, Wrong, Wrong, Wrong, Wrong].into(),
+            ),
         ];
 
         for (w, g, expected) in table {
             let got = score(w, g);
             assert_eq!(got, expected);
-            assert_eq!(got, decompress(compress(&got)));
+            // assert_eq!(got, decompress(compress(&got)));
         }
+    }
+
+    type ScoreArray = [LetterScore; 5];
+
+    impl From<ScoreArray> for Score {
+        fn from(value: ScoreArray) -> Self {
+            Score(compress(&value))
+        }
+    }
+
+    /// `compress` compresses a `Score` into a single byte.
+    /// This works out because log2(3^5) < 8.
+    ///
+    /// [`decompress`] reverses this process again.
+    #[allow(unused)]
+    fn compress(score: &ScoreArray) -> u8 {
+        let mut compressed = 0;
+        for s in score.iter().rev() {
+            compressed *= 3;
+            compressed += s.variant();
+        }
+        compressed
     }
 }
